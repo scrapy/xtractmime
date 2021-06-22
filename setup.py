caa56a173cd07b7853630281be2648aed799d523
Eugenio Lacuesta <1731933+elacuesta@users.noreply.github.com>
2021-06-22 04:22:23 -0300

    Project boilerplate and CI (#3)

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name="xtractmime",
+    version="0.0.0",
+    url="https://github.com/scrapy/xtractmime",
+    packages=["xtractmime"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Scrapy",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
