9f65cc69a12503e0c8e5df0a0786a5caf3b852b2
Akshay Sharma <akshaysharmajs@gmail.com>
2021-07-02 16:29:08 +0530

    Add main sniffing algo

--- xtractmime/__init__.py
+++ xtractmime/__init__.py
@@ -1,5 +1,11 @@
 __version__ = "0.0.0"
 from typing import Optional, Set, Tuple
+from xtractmime._utils import (
+    is_archive,
+    is_audio_video,
+    is_font,
+    is_image,
+)
 
 _APACHE_TYPES = [
     b"text/plain",
@@ -39,6 +45,21 @@ def _is_match_mime_pattern(
     return True
 
 
+def _find_unknown_mimetype(input_bytes: bytes, sniff_scriptable: bool):
+    # TODO
+    pass
+
+
+def _sniff_mislabled_binary(input_bytes: bytes):
+    # TODO
+    pass
+
+
+def _compare_feed_html(input_bytes: bytes):
+    # TODO
+    pass
+
+
 def extract_mime(
     body: bytes,
     *,
@@ -48,8 +69,33 @@ def extract_mime(
     extra_types: Optional[Tuple[Tuple[bytes, bytes, Set[bytes], str], ...]] = None,
 ) -> str:
     extra_types = extra_types or tuple()
-    # supplied_type = content_types[-1] if content_types else None
+    supplied_type = content_types[-1] if content_types else None
+    check_for_apache = http_origin and supplied_type in _APACHE_TYPES
+    resource_header = memoryview(body) if len(body) < 1445 else memoryview(body)[:1445]
 
-    # check_for_apache = http_origin and supplied_type in _APACHE_TYPES
-    # resource_header = memoryview(body) if len(body) < 1445 else memoryview(body)[:1445]
-    return "mimetype"
+    if supplied_type in (None, "unknown/unknown", "application/unknown", "*/*"):
+        _find_unknown_mimetype(body, not no_sniff)
+
+    if no_sniff:
+        return supplied_type
+
+    if check_for_apache:
+        return _sniff_mislabled_binary(body)
+
+    if supplied_type[-4:] is "+xml" or supplied_type in ("text/xml", "application/xml"):
+        return supplied_type
+
+    if supplied_type is "text/html":
+        return _compare_feed_html(body)
+
+    matched_type = is_image(resource_header)
+
+    if matched_type:
+        return matched_type
+
+    matched_type = is_audio_video(resource_header)
+
+    if matched_type:
+        return matched_type
+
+    return supplied_type
